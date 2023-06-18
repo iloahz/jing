4b20b1bce451958df59eb668c4bfb7a006520575
Li Zhao <iloahz@gmail.com>
2023-06-18 21:37:15 +0800

    2 column draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 
 export default function App() {
+  // return 2 horizontal columns, with green and red background
+  // columns shall fill the whole page
+  // columns shall have a width of 50% of the page
+  // columns shall have a height of 100% of the page
+  // columns shall have 0 margin, padding, border
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-row w-screen h-screen">
+      <div className="flex flex-col w-1/2 h-full bg-green-500"></div>
+      <div className="flex flex-col w-1/2 h-full bg-red-500"></div>
+    </div>
   )
 }
